e127d4804b527179d4dc7e5e6a7f9fe01c1ed749
KantaTamura <tkanta496@gmail.com>
2021-09-11 20:45:24 +0900

    disable post-output processing

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ void enableRawMode() {
     
     tcgetattr(STDIN_FILENO, &raw);
     raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
