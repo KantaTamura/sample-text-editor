b32529337cf2d7c25353ba634b297a9bacb042a7
KantaTamura <tkanta496@gmail.com>
2021-09-11 16:47:14 +0900

    disable raw on exit

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-    struct termios raw;
+    struct termios raw = orig_termios;
+    atexit(disableRawMode);
+    
     tcgetattr(STDIN_FILENO, &raw);
     raw.c_lflag &= ~(ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
