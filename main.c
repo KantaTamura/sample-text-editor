7794844dd635b8bffd76120c87e046d6be0fe1ec
KantaTamura <tkanta496@gmail.com>
2021-09-11 20:28:55 +0900

    disable c-s c-q

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ void enableRawMode() {
     atexit(disableRawMode);
     
     tcgetattr(STDIN_FILENO, &raw);
+    raw.c_iflag &= ~(IXON);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
